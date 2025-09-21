643800f83aad0176781845d8a3329f8624c989bf
seong hyeon <sonhyon86@gmail.com>
2025-09-21 17:53:01 +0900

    Add files via upload

---
+++ 02_인과성 분석.R
@@ -0,0 +1,14 @@
+#*[종속변수]
+head(Traffic_Accident)
+head(Traffic_Accident_cnt)
+
+#───────────────────────────────────────────────────────────────────────────────
+#*[독립변수]
+head(highway_structures_all)
+head(Highway_Traffic_Management_System_all)
+head(Traffic_Volume)
+head(Traffic_Volume_ver2)
+head(Annual_Average_Daily_Traffic)
+head(Drowsy_Driving)
+
+#─────────────────────────연도를 잘 맞춰서 분석해야함───────────────────────────
